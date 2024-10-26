e2518a7d641dc659fc026eff5422e1bc7633293c
Collin Phan <collincomms@gmail.com>
2024-10-27 04:15:05 +0700

    Update Global exception handler
    + add method to convert string into status code.
    
    Note: Due to some reason, error message no longer returned.

--- Backend/API/Middleware/ExceptionHandler/APIExceptionHandler.cs
+++ Backend/API/Middleware/ExceptionHandler/APIExceptionHandler.cs
@@ -1,4 +1,5 @@
-﻿using Backend.Cores.Commons;
+﻿using System.Net;
+using Backend.Cores.Commons;
 using Microsoft.AspNetCore.Diagnostics;
 
 namespace Backend.API.Middleware.ExceptionHandler
@@ -6,17 +7,36 @@ namespace Backend.API.Middleware.ExceptionHandler
     public class APIExceptionHandler : IExceptionHandler
     {
         private readonly ILogger<APIExceptionHandler> logger = null!; // Need more researching
+
         public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
         {
             HttpResponseData response = new HttpResponseData
             {
-                Message = exception.Message != null ? exception.Message : exception.ToString().Split(Environment.NewLine).First(),
+                Message = exception.Message != null ? exception.Message : "Something gone wrong and we don't know (Exception_Unknown)",
                 Data = exception.Data,
             };
-
+            
+            httpContext.Response.StatusCode = exception.Data.Contains("type") ? DetermineResponseStatusCode((exception.Data["type"] as string)!) : 500;
             await httpContext.Response.WriteAsJsonAsync(response, cancellationToken);
 
             return true;
+        }
+
+        private int DetermineResponseStatusCode(string errorType)
+        {
+            switch(errorType)
+            {
+                case "Invalid":
+                    return 400;
+                case "NotFound":
+                    return 404;
+                case "Unauthorized":
+                    return 401;
+                case "UnknownError":
+                    return 500;
+                default:
+                    return 200;
+            }
         }
     }
 }

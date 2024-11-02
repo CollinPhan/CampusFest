1374ace446e0814e16cefae0d74332ea3c7ff743
Collin Phan <collincomms+github@gmail.com>
2024-11-03 01:00:47 +0700

    Fix Regex
    +Fixed the email validation string
    + Removed debugging console log

--- Backend/API/Services/Implementation/AccountService.cs
+++ Backend/API/Services/Implementation/AccountService.cs
@@ -59,7 +59,7 @@ namespace Backend.API.Services.Implementation
             }
             
             // Checking for a valid email address.
-            if (!ValidationHelper.ValidateString(accountInfo.Email, @"(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])"))
+            if (!ValidationHelper.ValidateString(accountInfo.Email, @"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"))
             {
                 ExceptionGenerator.GenericServiceException<BaseServiceException>(
                     message: "Email address is invalid", 
@@ -149,8 +149,6 @@ namespace Backend.API.Services.Implementation
             Expression<Func<Account, bool>> predicate = x => (x.Username == username || x.Email == username) && x.Password == HashPassword(password);
 
             var entity = await accountRepo.FindFirstMatch(predicate, x => x.Role);
-            
-            Console.WriteLine(entity != null);
 
             if (entity == null)
             {
@@ -194,12 +192,6 @@ namespace Backend.API.Services.Implementation
             }    
 
             var result = await accountRepo.GetPaginated(page, page_size, includeProperty, filterExpression, sortByExpression, false, isDecending);
-
-            Console.WriteLine(username);
-            foreach (var s in result)
-            {
-                Console.WriteLine(s.Username.Contains(username));
-            }
 
             return mapper.Map<IEnumerable<Account>, IEnumerable<AccountDTO>>(result);
         }
